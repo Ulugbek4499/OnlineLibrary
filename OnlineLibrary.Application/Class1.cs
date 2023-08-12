b6ff6ac379f21a4c82c2c7083d8eb739013e2d97
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-12 18:32:40 +0500

    Add project files.

---
+++ OnlineLibrary.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineLibrary.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ OnlineLibrary.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineLibrary.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
