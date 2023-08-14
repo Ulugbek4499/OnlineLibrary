8489f3a97b5a5c2a049814c68d357a9fa9c61ca0
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-08-14 18:01:49 +0500

    save

--- OnlineLibrary.Application/Common/JWT/Service/RefreshToken.cs
+++ OnlineLibrary.Application/Common/JWT/Service/RefreshToken.cs
@@ -1,13 +1,43 @@
-﻿using OnlineLibrary.Application.Common.JWT.Interfaces;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using OnlineLibrary.Application.Common.Interfaces;
+using OnlineLibrary.Application.Common.JWT.Interfaces;
 using OnlineLibrary.Domain.Entites.Identity;
 
 namespace OnlineLibrary.Application.Common.JWT.Service
 {
     public class RefreshToken : IUserRefreshToken
     {
-        public ValueTask<UserRefreshToken> AddOrUpdateRefreshToken(UserRefreshToken refreshToken, CancellationToken cancellationToken = default)
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public RefreshToken(IApplicationDbContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async ValueTask<UserRefreshToken> AddOrUpdateRefreshToken(UserRefreshToken refreshToken, CancellationToken cancellationToken = default)
+        {
+            var foundRefreshToken = await _context.RefreshTokens.FirstOrDefaultAsync(
+                x=>x.UserName==refreshToken.UserName, cancellationToken);
+
+            if (foundRefreshToken is null)
+            {
+                await _context.RefreshTokens.AddAsync(refreshToken, cancellationToken);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return refreshToken;
+            }
+            else
+            {
+                foundRefreshToken.RefreshToken = refreshToken.RefreshToken;
+                foundRefreshToken.ExpiresTime = refreshToken.ExpiresTime;
+                _context.RefreshTokens.Update(foundRefreshToken);
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return refreshToken;
+            }
         }
 
         public ValueTask<User> AuthenAsync(LoginUserCommand user)
@@ -15,14 +45,22 @@ namespace OnlineLibrary.Application.Common.JWT.Service
             throw new NotImplementedException();
         }
 
-        public ValueTask<bool> DeleteUserRefreshTokens(string username, string refreshToken, CancellationToken cancellationToken = default)
+        public async ValueTask<bool> DeleteUserRefreshTokens(
+                              string username, string refreshToken, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            var foundRefreshToken=await _context.RefreshTokens.FirstOrDefaultAsync(
+                                 x=>x.UserName==username && x.RefreshToken==refreshToken, cancellationToken);
+            _context.RefreshTokens.Remove(foundRefreshToken);
+
+            return (await _context.SaveChangesAsync(cancellationToken))>0;
         }
 
-        public ValueTask<UserRefreshToken> GetSavedRefreshTokens(string userName, string refreshToken)
+        public async ValueTask<UserRefreshToken> GetSavedRefreshTokens(string userName, string refreshToken)
         {
-            throw new NotImplementedException();
+            var foundRefreshToken = await _context.RefreshTokens.FirstOrDefaultAsync(
+                x => x.UserName == userName && x.RefreshToken == refreshToken);
+
+            return foundRefreshToken;
         }
     }
 }
